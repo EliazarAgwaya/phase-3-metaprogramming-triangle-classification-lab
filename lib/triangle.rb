3bdb066c73e1d360daf03b215de01f1eab0175e0
EliazarAgwaya <eliazar.magero@student.moringaschool.com>
2022-08-23 09:58:16 +0300

    Done.

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,21 @@
 class Triangle
-  # write code here
-end
+
+  attr_accessor :x, :y, :z
+
+  def initialize(x, y, z)
+    @x = x
+    @y = y
+    @z = z
+  end
+
+  def kind
+    validate_triangle
+    if y == z && z == x
+      :equilateral
+    elsif x == y || y == z || x == z
+      :isosceles
+    else
+      :scalene
+    end
+    end
+  end
