fab03071b2e8f5528d4fd44c0f65d0069c41a1cc
EliazarAgwaya <eliazar.magero@student.moringaschool.com>
2022-08-23 10:01:09 +0300

    Done.

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -17,5 +17,20 @@ class Triangle
     else
       :scalene
     end
-    end
   end
+  def sides_greater_than_zero?
+    x > 0 && y > 0 && z > 0
+  end
+
+  def valid_triangle_inequality?
+    x + y > z && x + z > y && y + z > x
+  end
+
+  def validate_triangle
+    raise TriangleError unless sides_greater_than_zero? && valid_triangle_inequality?
+  end
+
+  class TriangleError < StandardError
+
+  end
+end
